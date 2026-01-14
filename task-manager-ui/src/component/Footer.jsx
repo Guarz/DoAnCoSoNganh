fd32c4f62e492b12bcaa3ada99149311c6f8b189
Guarz1a <dotranquanganh123@gmail.com>
2026-01-14 18:21:21 +0700

    .

---
+++ task-manager-ui/src/component/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer>
+      <div class="container footer-layout">
+        <div class="footer-left">
+          <p>Địa chỉ: CS2 - Học Viện Hàng không Việt Nam</p>
+          <p>Email: namanhembanhoa@gmail.com</p>
+        </div>
+        <div class="footer-right">
+          <p>Điện thoại: 0123456789</p>
+          <p>© 2025 NĂM ANH EM BÁN HOA. Bảo lưu mọi quyền.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ task-manager-ui/src/component/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h2>Header here</h2>
+    </header>
+  );
+}
+
+export default Header;
