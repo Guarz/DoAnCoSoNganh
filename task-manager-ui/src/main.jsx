fd1c1ea9e0666705b92d8a588e8c3d2911e04ca7
Guarz1a <dotranquanganh123@gmail.com>
2026-03-17 21:04:39 +0700

    .

--- task-manager-ui/src/main.jsx
+++ task-manager-ui/src/main.jsx
@@ -1,13 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 // import React from 'react'
 // import ReactDOM from 'react-dom/client'
 // import ProductList from './pages/User/ProductList'
@@ -15,6 +15,6 @@
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
-//     <ProductList /> 
+//     <ProductList />
 //   </React.StrictMode>,
 // )
