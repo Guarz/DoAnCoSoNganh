355ca13133ccc4cc4b0ca3fac07067abe1d89279
Guarz1a <dotranquanganh123@gmail.com>
2026-04-01 11:00:52 +0700

    .

--- task-manager-api/app/Models/SanPham.php
+++ task-manager-api/app/Models/SanPham.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// Import các model liên quan
 use App\Models\LoaiSP;
 use App\Models\AnhSP;
 use App\Models\ChiTietSanPham;
@@ -30,41 +29,26 @@ class SanPham extends Model
         'IdAnh'
     ];
 
-    // =============================
-    // Quan hệ loại sản phẩm
-    // =============================
     public function LoaiSP()
     {
         return $this->belongsTo(LoaiSP::class, 'IdLoai', 'IdLoai');
     }
 
-    // =============================
-    // Quan hệ ảnh sản phẩm
-    // =============================
     public function AnhSP()
     {
         return $this->belongsTo(AnhSP::class, 'IdAnh', 'IdAnh');
     }
 
-    // =============================
-    // Quan hệ chi tiết sản phẩm (giá, size)
-    // =============================
     public function ChiTietSanPham()
     {
         return $this->belongsTo(ChiTietSanPham::class, 'IdCT', 'IdCT');
     }
 
-    // =============================
-    // Quan hệ chi tiết giỏ hàng
-    // =============================
     public function ChiTietGioHang()
     {
         return $this->hasMany(ChiTietGioHang::class, 'IdSP', 'IdSP');
     }
 
-    // =============================
-    // Quan hệ chi tiết đơn hàng
-    // =============================
     public function ChiTietDonHang()
     {
         return $this->hasMany(ChiTietDonHang::class, 'IdSP', 'IdSP');
